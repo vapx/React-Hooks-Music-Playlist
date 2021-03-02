eb28d0b1c82e7a9e1dce580301af5e8a03c3e556
vapx <g.edmon@gmail.com>
2021-03-02 13:17:01 +0800

    Decided to learn how to make my own api using firebase and nodejs. I'll apply it later to my reactjs

--- src/App.js
+++ src/App.js
@@ -7,13 +7,6 @@ import axios from "axios";
 function App() {
 
   const [ music,setMusic ] = useState("");
-
-  function getMusic(){
-    axios.get("https://genius.p.rapidapi.com/artists/16775/songs")
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-  }
-
   useEffect(() => {
     getMusic();
   },[])
